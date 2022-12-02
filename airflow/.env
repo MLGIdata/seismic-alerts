f25f00531d8e7db7485d9a6b693f401910034d62
IDell49 <ivandellanque01@gmail.com>
2022-12-02 03:06:12 -0300

    Agregue scripts JP/MX/US y locations/us-states a data

---
+++ airflow/.env
@@ -0,0 +1,2 @@
+AIRFLOW_IMAGE_NAME=apache/airflow:2.4.2
+AIRFLOW_UID=50000
---
+++ airflow/dags/US.py
@@ -0,0 +1,35 @@
+from airflow import DAG
+from datetime import datetime, timedelta
+from airflow.operators.python_operator import PythonOperator
+import boto3
+from UShourly import main,pushS3
+
+default_args={
+    'owner': 'Airflow',
+    'start_date': datetime(2022,11,20),
+    'retries': 3,
+    'retry_delay': timedelta(seconds=3)
+}
+
+with DAG (
+    "US_Hourly_Upload",
+    default_args = default_args,
+    schedule_interval = '@daily',
+    catchup=False
+    ) as dag:
+
+    extract_and_transform = PythonOperator(
+        task_id = "extract_and_transform",
+        python_callable = main
+    )
+
+    load = PythonOperator(
+        task_id = "load",
+        python_callable = pushS3,
+        op_kwargs = {
+            'path': 'dags/data/usa_usgs.csv',
+            'id': 'USAdata'
+        }
+    )
+
+    extract_and_transform >> load 
